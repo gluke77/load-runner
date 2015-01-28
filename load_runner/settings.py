ebdc3927d94d279f558c56f69dab7367f6a822fa
Illia Khudoshyn <ikhudoshyn@mirantis.com>
2015-01-28 14:35:53 +0200

    Settings take values from environment

--- load_runner/settings.py
+++ load_runner/settings.py
@@ -12,25 +12,30 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-CONTROLLER_IP = '172.16.0.2'
-OS_AUTH_URL = 'http://%s:5000/v2.0/' % CONTROLLER_IP
-OS_SERVICE_ENDPOINT = 'http://%s:35357/v2.0/' % CONTROLLER_IP
-OS_USERNAME = 'admin'
-OS_TENANT = 'admin'
-OS_PASSWORD = 'admin'
-OS_TOKEN = 'PKUiCQHi'
-SPAWN_DELAY = 0
-ACTIVATION_TIMEOUT = 120
-BOOT_TIMEOUT = 300
-TEST_TIMEOUT = 120
+from os import environ as env
 
-MANAGEMENT_NETWORK_ID = '9398a44e-c170-4e50-affc-e4f321a67069'
-MANAGEMENT_NET_NAME = 'mgmtnet'
-MANAGEMENT_CIDR = '192.168.111.0/24'
-PRIVATE_CIDR = '192.168.0.0/24'
-FLAVOR_ID = '6'
-IMAGE_ID = '0e6cfb31-5bd3-420f-b9a9-9494d5f3907a'
-ROOT_DIR = '/tmp'
+
+CONTROLLER_IP = env.get('CONTROLLER_IP')
+
+OS_TENANT_NAME = env.get('OS_TENANT_NAME', 'demo')
+OS_USERNAME = env.get('OS_USERNAME', 'admin')
+OS_PASSWORD = env.get('OS_PASSWORD', 'pass')
+OS_AUTH_URL = env.get('OS_AUTH_URL', 'http://{}:5000/v2.0/'.format(CONTROLLER_IP))
+OS_SERVICE_ENDPOINT = env.get('OS_SERVICE_ENDPOINT', 'http://{}:35357/v2.0/'.format(CONTROLLER_IP))
+OS_TOKEN = env.get('OS_TOKEN')
+
+SPAWN_DELAY = int(env.get('SPAWN_DELAY', 0))
+ACTIVATION_TIMEOUT = int(env.get('ACTIVATION_TIMEOUT', 300))
+BOOT_TIMEOUT = int(env.get('BOOT_TIMEOUT', 300))
+TEST_TIMEOUT = int(env.get('TEST_TIMEOUT', 300))
+
+MANAGEMENT_NETWORK_ID = env.get('MANAGEMENT_NETWORK_ID')
+MANAGEMENT_NET_NAME = env.net('MANAGEMENT_NET_NAME', 'private')
+MANAGEMENT_CIDR = env.get('MANAGEMENT_CIDR')
+PRIVATE_CIDR = env.get('PRIVATE_CIDR', '192.168.0.0/24')
+FLAVOR_ID = env.get('AGENT_FLAVOR_ID')
+IMAGE_ID = env.get('AGENT_IMAGE_ID')
+ROOT_DIR = env.get('ROOT_DIR', '/tmp')
 
 UPDATE_NEUTRON_QUOTAS = False  # Set to False for Contrail testing
 USE_DHCP = True  # Set to True for Contrail testing
